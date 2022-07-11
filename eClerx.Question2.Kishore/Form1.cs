9002a6259c8be3a0c412064f0b89b8f0d063527c
EerallaKishore <62879121+EerallaKishore@users.noreply.github.com>
2022-07-11 19:42:17 +0530

    Update Form1.cs

--- eClerx.Question2.Kishore/Form1.cs
+++ eClerx.Question2.Kishore/Form1.cs
@@ -28,31 +28,38 @@ namespace eClerx.Question2.Kishore
         }
         private SqlConnection con = null;
         private SqlDataAdapter adapter = null;
+        private SqlCommand cmd = null;
         private DataSet ds = null;
         private void BtnGetData_Click(object sender, EventArgs e)
         {
             using (con = new SqlConnection(ConfigurationManager.ConnectionStrings["HR"].ConnectionString))
             {
-                using (adapter = new SqlDataAdapter("Select * from Customer", con))
+                using (cmd = new SqlCommand("Select * from Customer where City=@CustomerCity", con)) //take Hyderabad City and check all Validation
                 {
+                    cmd.Parameters.AddWithValue("@CustomerCity", TxtCustomerCity.Text);
+                    adapter = new SqlDataAdapter(cmd);
                     ds = new DataSet();
                     adapter.Fill(ds, "Customer");
                     DataView dv = ds.Tables["Customer"].DefaultView; 
                     if (ComboSortBy.SelectedIndex == 0)
                     {
-                        dv.RowFilter = "CreditLimit>=0 and CreditLimit<=90000 ";
+                        dv.sort = "CreditLimit ASC";
+                        //dv.RowFilter = "CreditLimit>=0 and CreditLimit<=90000";
                     }
                     else if (ComboSortBy.SelectedIndex == 1)
                     {
-                        dv.RowFilter = "CreditLimit<=90000 and CreditLimit>=0 ";
+                        dv.sort = "CreditLimit DESC";
+                        //dv.RowFilter = "CreditLimit<=90000 and CreditLimit>=0 ";
                     }
                     else if (ComboSortBy.SelectedIndex == 2)
                     {
-                        dv.RowFilter = "select * from Customer order by CustomerCity ASC";
+                        dv.sort = "CustomerName ASC";
+                        //dv.RowFilter = "select * from Customer order by CustomerName ASC";
                     }
                     else
                     {
-                        dv.RowFilter = "select * from Customer order by CustomerCity DESC";
+                        dv.sort = "CustomerName DESC";
+                        //dv.RowFilter = "select * from Customer order by City DESC";
                     }
                      GridCustomer.DataSource = dv;
                 }
